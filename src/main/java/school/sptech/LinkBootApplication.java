2a54492c1c3edda027cca41a162ea8be2659397c
Guilherme Borin Galeno <guilherme.galeno@sptech.school>
2025-04-15 00:49:05 -0300

    feat: adiciona estrutura inicial do projeto Spring Boot

---
+++ src/main/java/school/sptech/LinkBootApplication.java
@@ -0,0 +1,13 @@
+package school.sptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkBootApplication.class, args);
+	}
+
+}
